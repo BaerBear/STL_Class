ae33de3cbdf1588f3a9c5875645f5f7fbe6b130c
BaerBear <turbogun03@gmail.com>
2026-03-16 15:20:31 +0900

    class 템플릿을 코딩해봤다.

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -6,7 +6,7 @@
 // 프로젝트 설정 - C++언어표준 - /std:c++latest
 //				- C/C++언어 - SDL검사 - 아니요
 //---------------------------------------------------------------------------------------------------------------------
-// 
+// 다음 시간 멤버 설명부터 다시
 //----------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <random>
@@ -19,25 +19,45 @@
 std::default_random_engine dre{};
 std::uniform_int_distribution uid{ 0, 9999 };
 
+template <class T, int N>
+class myArray {
+public:
+	size_t size() const {
+		return N;
+	}
+
+	int& operator[] (int idx) {
+		return a[idx];
+	}
+
+	T* begin() {
+		return &a[0];
+	}
+
+	T* end() {
+		return &a[N];
+	}
+
+private:
+	T a[N];
+
+};
+
 //--------
 int main()
 //--------
 {
-	// [문제] 랜덤값을 갖는 int 1000개를 만들어라.
-	// int[1000] 이런 자료구조는 앞으로는 쓰지 말 것. (int a[1000])
-	// new int[1000] 말고는 쓸 곳이 없다.
-	// 얘는 본인의 크기도 모르고 아무것도 모른다
-	// 대체할 수 있는 더 좋은 것이 있기 때문. 
-	// 대체할 수 있다는 것 - 메모리 덜 쓴다, 시간이 덜걸린다.
-	// 얘는 시간/공간의 zero-overhead.
-	// array<T, N>
-	// int a[1000]           // 자료형은 int[N]인데, 이름 위치가 비정상.
-
-	std::array<int, 1000> a;
+	myArray<int, 1000> a;
+	std::cout << "a가 사용하는 메모리 - " << sizeof a << std::endl;	// 메모리 손해 x. 똑같이 4000바이트
+	// 표준 자료구조. a. 찍어보면? 여러 함수를 제공
 
 	// 여기는 초기화가 아니라 할당하는 작업임.
-	for (int& num : a) {
+	// 이것도 슈가임.
+	/*for (int& num : a) {
 		num = uid(dre);
+	}*/
+	for (int i = 0; i < a.size(); ++i) {
+		a[i] = uid(dre);
 	}
 
 	for (int num : a) {
@@ -47,7 +67,7 @@ int main()
 	// [문제] 가장 작은 값과 큰 값을 찾아 화면에 출력하라.
 	// syntatic sugar 인 structured binding을 사용하면 pair를 이렇게 쓸 수 있다.
 	// std::pair<int*, int*> res = std::minmax_element(std::begin(a), std::end(a));
-	auto [minPos, maxPos] = std::minmax_element(std::begin(a), std::end(a));
+	auto [minPos, maxPos] = std::minmax_element(a.begin(), a.end());
 	std::cout << "\n가장 작은 값: " << *minPos << " / 가장 큰 값: " << *maxPos << std::endl;
 
 	save("메인.cpp");
