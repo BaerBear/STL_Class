b2463b3757c9947a1d402da42f8ef494eeb971be
BaerBear <turbogun03@gmail.com>
2026-03-17 16:10:53 +0900

    '\n'이 속도가 훨씬 빠르네?

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -23,8 +23,10 @@ int main()
 {
 	// [문제] 랜덤 int값 천만개를 다음과 같이 "int천만개.txt" 파일에 저장해였다.
 	std::ofstream fout{ "int천만개.txt" };
-	for (int i = 0; i < 1000'0000; ++i) {		// 119,828,581 bytes
-		fout << uid(dre) << std::endl;
+	for (int i = 0; i < 1000'0000; ++i) {			// 119,828,581 bytes
+		fout << uid(dre) << '\n';					// endl과 '\n'을 써봤을 때 endl의 속도가 느림.
+													// endl은 버퍼를 강제로 비우는 기능까지 수행.
+													// '\n'은 단순히 줄만 바꾸고 버퍼를 바로 비우지 않아서 속도가 훨씬 빠름.
 	}
 
 	save("메인.cpp");
