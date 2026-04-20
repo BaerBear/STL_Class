690bb6e1b44aab0dbaccb9e8f3e81aa10360f80f
BaerBear <turbogun03@gmail.com>
2026-04-20 14:44:51 +0900

    벡터에 대한 설명과 함께 시작

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -22,6 +22,18 @@
 
 extern bool 관찰;			// 관찰하려면 true
 
+// vector는 bool을 제외하면 contiguous하게 저장된다.
+// 시작번지 + offset을 계산해서 element에 접근할 수 있다.
+// element를 가르키는 포인터를 함수의 인자로도 쓸 수 있다. (배열의 원소를 필요로하는 곳에	vector의 원소를 포인터로 전달할 수 있다.)
+// 필요에 따라서 메모리가 늘어난다. 그래서 컴파일 타임에 공간이 정해지는 array보다 공간이 더 필요하다.
+// dynamic하게 메모리가 확장(expanded)된다. 미리 확보한 메모리가 다 차면 확장된다.
+// 재할당은 비용이 굉장히 많이 들어가는 동작이기 때문에 reserve()를 활용해서 미리 공간을 확보해주면 좋다.
+// Random access -> constant O(1)
+// 끝에있는 원소에 추가하거나 지우거나 -> amorized constant 0(1)
+// 중간에 추가하거나 지우거나 -> linear O(n)
+// 그러니까 벡터는 위에 두개만 써서 사용하는거야.
+// 핵심 함수는 push_back과 emplace_back
+//
 
 //--------
 int main()
@@ -30,24 +42,6 @@ int main()
 	SetConsoleOutputCP(CP_UTF8);
 	save("메인.cpp");
 
-	std::vector<ZString> v;
+	std::vector<int> v;
 
-	// [문제] "메인.cpp"에 있는 모든 단어를 v에 저장하라.
-
-	std::ifstream in{ "메인.cpp" };
-	if (not in)
-		return 20240420;
-
-	ZString zs;
-	while (in >> zs) {
-		v.push_back(zs);
-	}
-
-	std::sort(v.begin(), v.end(), [](const ZString& a, const ZString& b) {
-		return a.size() < b.size();
-		});
-
-	for (const ZString& zs : v) {
-		std::cout << zs << '\n';
-	}
 }
