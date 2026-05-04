916c91165147e7d056e21161393af4eca33e3490
BaerBear <turbogun03@gmail.com>
2026-05-04 14:32:22 +0900

    vector vs list 이거 보여주려고 어그로 끌었다.

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -11,6 +11,35 @@
 // Sequence Container
 // - array<T, N> - 유일하게 컴파일 타임에 size 결정 - STACK, DATA
 // - vector<T> - dynamic (size) array - free-store에 data 관리
+// - list<T> - 상수시간에 데이터를 추가/삭제 하기 위한 컨테이너. "아무 곳에서나 상수시간"에 작동한다. O(1)
+//		대신에 random access를 지원할 수는 없다. 미치지 않았다면 이중 연결 리스트로 구현했을거다.
+//		단일 연결리스트와 비교해 본다면, 이중연결리스는 양쪽으로 이터레이션을 지원하는 대신 공간을 더 사용한다. 포인터가 2개 필요함.
+//		merge, splice, remove, reverse, unique, sort 
+// 
+// vector vs list
+// - vector
+//	armortized O(1) 시간에 push_back(), pop_back()
+//  contiguous memory
+//  O(1) 시간에 random access 가능
+//  벡터를 쓰는 이유는 ? cache hit 가능성이 훨씬 높아지기 때문에 속도 차이가 극명할 수 있다.
+//	벡터가 ZString을 한개 담기위해 필요한 메모리는 24Byte
+// 
+// - list
+//  O(1) 시간에 insert(), erase()
+//  free-store에서 다음 노드를 포인팅하고 있음. 추가 삭제가 빠름
+//  random access 불가능. 순차적으로 접근해야함. O(n)
+//  리스트는 구조적으로 cache hit의 가능성이 현저히 낮다.
+//  리스트가 ZString을 한개 담기위해 필요한 메모리는 24Byte + 노트 포인터 2개(8Byte * 2) = 40Byte
+//  리스트는 포인터 크기의 메모리를 희생하면서도 벡터보다 free-store에 더 많이 저장할 수 있다.
+// 
+//	벡터가 free-store의 50%를 사용한다고 가정할 때, 용량이 다 찼다면 1.5배 늘린다고 했을 때 연속된 공간을 더 찾을 가능성이 없다.
+//  하지만 리스트는 곳곳에 존재할 수 있기 때문에 더 많이 저장할 수 있다.
+// 
+//  하지만 이 둘의 장점을 합치고 싶은데 ?? -> deque.
+//  블럭 단위로 메모리를 할당해서 벡터처럼 연속된 공간을 제공하면서도 리스트처럼 빠른 추가/삭제가 가능하다.
+//  
+// 
+// 
 //----------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <vector>
