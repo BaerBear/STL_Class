5f588483399d9fba6eb328953ca96d30436ff255
BaerBear <turbogun03@gmail.com>
2026-06-02 15:34:12 +0900

    my_copy로 시작

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -1,5 +1,5 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// 2026년 1학기 STL 월56화78			6월 1일			(12주 2일)
+// 2026년 1학기 STL 월56화78			6월 2일			(13주 1일)
 //--------------------------------------------------------------------------------------------------------------------
 // 컴파일 환경 - Release / x64
 // VS 버전 - 17.14.27 (February 2026)
@@ -12,13 +12,19 @@
 //----------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <set>
-#include <fstream>
-#include <algorithm>
 #include <vector>
 #include "save.h"
 #include "ZString.h"
 
 extern bool 관찰;			// 관찰하려면 true
+
+template<class 원본, class 복사본>
+void my_copy(원본 beg, 원본 end, 복사본 out) 
+{
+	while (beg != end) {
+		*beg++ = *out++; // 컨테이너에 관계없이 항상 실행되는 코드
+	}
+}
 
 //--------
 int main()
@@ -26,41 +32,4 @@ int main()
 {
 	save("메인.cpp");
 
-	// "이상한 나라의 앨리스.txt"의 단어를 set<ZString>에 저장하라.
-	std::ifstream in{ "이상한 나라의 앨리스.txt" };
-
-	if (not in) {
-		std::cout << "파일을 살펴보세요" << std::endl;
-		return 20260601;
-	}
-	
-	std::multiset<ZString> s{ std::istream_iterator<ZString>{in}, {} };
-
-	for(const ZString& zs : s) {
-		std::cout << zs << "  ";
-	}
-	std::cout << std::endl;
-	
-	std::cout << "단어 개수 - " << s.size() << std::endl;
-
-	// [문제] 단어를 입력받아 있다면 몇개 있는지
-	// 없다면 없다고 출력하라.
-
-	while (true) {
-		std::cout << "찾을 단어? : ";
-		ZString word;
-		std::cin >> word;
-		
-
-		auto [하한, 상한] = s.equal_range(word);
-		// pair로 받아 코딩해 보세요
-		
-		if (하한 == 상한) {
-			std::cout << "없는 단어" << std::endl;
-		}
-		else {
-			std::cout << std::distance(하한, 상한) << "개" << std::endl;
-			// contiguos 메모리가 아니므로 반드시 distance로 구해야 함.
-		}
-	}
 }
