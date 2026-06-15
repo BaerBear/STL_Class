dc142e57f0665d0db56e88f4995d75d43fcc65fd
BaerBear <turbogun03@gmail.com>
2026-06-15 13:40:54 +0900

    1~100의 수 준비 및 셔플

--- STL_Class/메인.cpp
+++ STL_Class/메인.cpp
@@ -1,5 +1,5 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// 2026년 1학기 STL 월56화78			6월 9일			(14주 1일)
+// 2026년 1학기 STL 월56화78			6월 15일			(14주 2일)
 // 6/22 기말시험(15/2) - 제일 마지막 시험
 //--------------------------------------------------------------------------------------------------------------------
 // 컴파일 환경 - Release / x64
@@ -9,12 +9,20 @@
 //---------------------------------------------------------------------------------------------------------------------
 // STL Algorithms
 // - 정렬 관련 알고리즘 - 복잡도 순서대로
+// - partition - 복잡도가 제일 낮음
+// - nth_element
+// - partial_sort
+// - sort
+// - stable_sort - 제일 복잡함
 //----------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <ranges>
+#include <numeric>
+#include <print>
 #include "save.h"
 #include "ZString.h"
 
@@ -27,40 +35,14 @@ int main()
 //--------
 {
 	save("메인.cpp");
-	std::ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (not in) {
-		std::cout << "파일 오류" << '\n';
-		return 20260609;
-	}
-	std::vector<ZString> v{ std::istream_iterator<ZString>{in}, {} };
-	std::cout << "모두 " << v.size() << "개의 단어를 읽었다." << std::endl;
 
-	std::sort(v.begin(), v.end());
-	auto newEnd = std::unique(v.begin(), v.end());
-	v.erase(newEnd, v.end());
-	std::cout << "중복을 제거한 단어 개수 - " << v.size() << std::endl;
+	// 1부터 100까지 숫자를 준비한다.
+	std::vector<int> v(100);
+	std::ranges::iota(v, 1);		// range 설명은 잠시뒤에
+	std::ranges::shuffle(v, dre);
 
-	std::shuffle(v.begin(), v.end(), dre);
-
-	// [문제] 길이가 홀수인 것과 짝수인 것으로 분리하라.
-	// 앞에는 홀수 뒤에는 짝수
-	// 홀수 앞에 짝수가 있으면 안된다.
-	// 홀수와 짝수가 각각 몇 개인지 출력하라.
-	auto p = std::partition(v.begin(), v.end(), [](const ZString& a) {
-		return *a.begin() == 'a';	// a로 시작하는 단어가 앞에 오게 분리할 수도 있다.
-									// 문제를 낸다면 단어에 p가 들어가는 단어와 아닌것으로 나눠라. 같은 문제들
-		});
-
-	std::cout << "길이가 홀수인 단어 - " << std::distance(v.begin(), p) << "개" << std::endl;
-	for (auto i = v.begin(); i != p; ++i) {
-		std::cout << *i << " ";
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "길이가 짝수인 단어 - " << std::distance(p, v.end()) << "개" << std::endl;
-	for (auto i = p; i != v.end(); ++i) {
-		std::cout << *i << " ";
+	for (int num : v) {
+		std::print("{:4}", num);
 	}
 	std::cout << std::endl;
 }
